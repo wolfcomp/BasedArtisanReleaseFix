7a9b07db8ec4f1e036aab236c70bb7ec100497ac
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-04-04 15:16:12 +0200

    add some more logic for automatic patching

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 # Changelog
 
+## [1.0.4] (2025-04-04)
+Now with automatic patching
+
+## [1.0.3] (2025-04-03)
+Fix a typo that was untypod in my code
+
+## [1.0.2] (2025-04-01)
+Fix reflection
+
 ## [1.0.1] (2025-03-29)
 Fix binding flags
 
--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,7 @@ using System.Runtime.Loader;
 using Dalamud.Game.Command;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
+using Lumina.Excel.Sheets;
 
 namespace BasedArtisanReleaseFix
 {
@@ -15,7 +16,9 @@ namespace BasedArtisanReleaseFix
         static IPluginLog? Log { get; set; }
         static IFramework? Framework { get; set; }
         static IChatGui? Chat { get; set; }
-        public Main(IDalamudPluginInterface pluginInterface, ICommandManager commandManager, IPluginLog log, IFramework framework, IChatGui chat)
+        static IClientState? State { get; set; }
+        private static uint[] _crafterIds = [];
+        public Main(IDalamudPluginInterface pluginInterface, ICommandManager commandManager, IPluginLog log, IFramework framework, IChatGui chat, IClientState state, IDataManager data)
         {
             var commandInterface = new CommandInfo((_, args) =>
             {
@@ -23,18 +26,42 @@ namespace BasedArtisanReleaseFix
                     FixArtisan();
                 else
                     _installedPluginsList = null;
-            });
-            commandInterface.HelpMessage = "fix - Fix Artisan\nclearfix - Clear installed plugins list and fix Artisan";
+            })
+            {
+                HelpMessage = "fix - Fix Artisan\nclearfix - Clear installed plugins list and fix Artisan"
+            };
             commandManager.AddHandler("/barf", commandInterface);
             Instance = pluginInterface;
             CommandManager = commandManager;
             Log = log;
             Framework = framework;
             Chat = chat;
-            // FixArtisan();
+            State = state;
+            State.ClassJobChanged += StateOnClassJobChanged;
+            foreach (var classJob in data.GetExcelSheet<ClassJob>())
+            {
+                if (classJob.ClassJobCategory.RowId == 33) _crafterIds = _crafterIds.Append(classJob.RowId).ToArray();
+            }
+            FixArtisan(true);
         }
 
-        private static void FixArtisan()
+        private void StateOnClassJobChanged(uint classjobid)
+        {
+            if (_crafterIds.Contains(classjobid)) FixArtisan(true);
+        }
+
+        const string AlreadyFixed = "[BasedArtisanReleaseFix] Artisan is not a staging build or has already been fixed, skipping fix.";
+        private static readonly string[] FixedStrings = [
+            "[BasedArtisanReleaseFix] Artisan has been fixed. Suck it up Taurenkey! >:D",
+            "[BasedArtisanReleaseFix] Artisan has been patched and fixed for you.",
+            "[BasedArtisanReleaseFix] I really wish I didn't need to make this plugin and they just accepted the fix that was in pull request #136...",
+            "[BasedArtisanReleaseFix] Artisan has been fixed. #136 wasn't that bad after all huh?",
+            "[BasedArtisanReleaseFix] Artisan has been fixed. You're welcome, I guess.",
+            "[BasedArtisanReleaseFix] Artisan has been fixed. It's just a matter of time before they release a new version with the fix due to this plugin.",
+            "[BasedArtisanReleaseFix] Artisan has been fixed. Dalamud will only have breaking changes if it needs to be fixed. Stop being childish and not allowing staging builds to be used."
+        ];
+
+        private static void FixArtisan(bool silent = false)
         {
             try
             {
@@ -43,6 +70,14 @@ namespace BasedArtisanReleaseFix
                 if (instance == null) throw new Exception("Failed to get DalamudInterface instance");
 
                 var pluginType = plugin.GetType();
+
+                if (!(bool)pluginType.Assembly.GetType("DalamudInfo")!.GetField("IsStaging", BindingFlags.Static | BindingFlags.Public)!.GetValue(null)!)
+                {
+                    if (silent) return;
+                    Log!.Info(AlreadyFixed);
+                    Chat!.Print(AlreadyFixed);
+                    return;
+                }
 
                 // init fields
                 var fields = pluginType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
@@ -59,18 +94,17 @@ namespace BasedArtisanReleaseFix
                         field.SetValue(plugin, obj);
                         if (field.Name == "cw") addWindow.Invoke(windowSystem.GetValue(plugin), [obj]);
                     }
-                
+
                     if (field.Name == "PluginUi")
                     {
                         var obj = field.GetValue(plugin);
                         field.FieldType.GetProperty("OpenWindow", BindingFlags.Instance | BindingFlags.Public)?.SetValue(obj, 1);
                     }
                 }
-                
+
                 var types = pluginType.Assembly.GetTypes();
                 foreach (var type in types)
                 {
-                    Log.Info(type.Name);
                     // init static types
                     if (type.Name is "CraftingProcessor" or "EnduranceCraftWatcher")
                     {
@@ -79,7 +113,7 @@ namespace BasedArtisanReleaseFix
                         else
                             throw new Exception($"Failed to get Setup method of {type.Name}");
                     }
-                
+
                     if (type.Name is "ConsumableChecker" or "Endurance" or "IPC" or "RetainerInfo"
                         or "CraftingListContextMenu")
                     {
@@ -89,7 +123,7 @@ namespace BasedArtisanReleaseFix
                         else
                             throw new Exception($"Failed to get Init method of {type.Name}");
                     }
-                
+
                     if (type.Name == "DalamudInfo")
                     {
                         if (type.GetField("IsStaging", BindingFlags.Static | BindingFlags.Public) is { } info)
@@ -97,7 +131,7 @@ namespace BasedArtisanReleaseFix
                         else
                             throw new Exception($"Failed to get IsStaging field of {type.Name}");
                     }
-                
+
                     // init windows
                     if (type.Name is "RecipeWindowUI" or "ProcessingWindow" or "QuestHelper")
                         addWindow.Invoke(windowSystem.GetValue(plugin), [Activator.CreateInstance(type)]);
@@ -115,7 +149,7 @@ namespace BasedArtisanReleaseFix
                 if (methodPropertyInfo == null) throw new Exception("Failed to get OnFrameworkUpdate method");
                 var eventInfo = eventPropertyInfo.PropertyType.GetEvent("Update", BindingFlags.Instance | BindingFlags.Public);
                 if (eventInfo == null) throw new Exception("Failed to get Update event");
-                eventInfo.AddEventHandler(eventPropertyInfo.GetValue(null), 
+                eventInfo.AddEventHandler(eventPropertyInfo.GetValue(null),
                     methodPropertyInfo.CreateDelegate(eventInfo.EventHandlerType!, plugin));
                 eventPropertyInfo = svcType.GetProperty("ClientState", BindingFlags.Static | BindingFlags.Public);
                 if (eventPropertyInfo == null) throw new Exception("Failed to get ClientState property");
@@ -144,25 +178,19 @@ namespace BasedArtisanReleaseFix
 
                 pluginType.GetMethod("ConvertCraftingLists", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(plugin, null);
 
-                Log.Info("Artisan fixed! Suck it up Taurenkey! >:D");
-                Chat.Print("Artisan fixed! Suck it up Taurenkey! >:D");
+                var message = FixedStrings[Random.Shared.Next(FixedStrings.Length)];
+
+                Log!.Info(message);
+                Chat!.Print(message);
             }
             catch (Exception ex)
             {
                 Log!.Error(ex, "Failed to fix Artisan");
             }
-
-            // Old way
-            // var type = types.FirstOrDefault(t => t.Name == "DalamudInfo");
-            // if (type == null) Log!.Info("Failed to find DalamudInfo");
-            // else type.GetField("IsStaging", BindingFlags.Static | BindingFlags.Public)?.SetValue(null, false);
-            // var newPlugin = plugin.GetType().GetConstructor([instance.GetType()])?.Invoke([instance]);
-            // if (newPlugin == null) return;
-            // SetLoadedPlugin("Artisan", newPlugin);
         }
 
         #region Reflection Helpers
-        // Joinked from SimpleHeels
+        // Joinked from SimpleHeels with some tweaks to make it work how I need it to
         private static IList? _installedPluginsList;
         private static bool TryGetLoadedPlugin(string internalName, [NotNullWhen(true)] out IDalamudPlugin? plugin, [NotNullWhen(true)] out object? localPlugin)
         {
@@ -172,7 +200,7 @@ namespace BasedArtisanReleaseFix
 
             PropertyInfo? internalNameProperty = null;
 
-            foreach (var v in _installedPluginsList)
+            foreach (var v in _installedPluginsList!)
             {
                 internalNameProperty ??= v?.GetType().GetProperty("InternalName");
 
@@ -194,28 +222,25 @@ namespace BasedArtisanReleaseFix
 
         private static void PopulatePlugins()
         {
-            if (_installedPluginsList == null)
-            {
-                var dalamudAssembly = typeof(IDalamudPluginInterface).Assembly;
-                var service1T = dalamudAssembly.GetType("Dalamud.Service`1");
-                if (service1T == null) throw new Exception("Failed to get Service<T> Type");
-                var pluginManagerT = dalamudAssembly.GetType("Dalamud.Plugin.Internal.PluginManager");
-                if (pluginManagerT == null) throw new Exception("Failed to get PluginManager Type");
+            var dalamudAssembly = typeof(IDalamudPluginInterface).Assembly;
+            var service1T = dalamudAssembly.GetType("Dalamud.Service`1");
+            if (service1T == null) throw new Exception("Failed to get Service<T> Type");
+            var pluginManagerT = dalamudAssembly.GetType("Dalamud.Plugin.Internal.PluginManager");
+            if (pluginManagerT == null) throw new Exception("Failed to get PluginManager Type");
 
-                var serviceInterfaceManager = service1T.MakeGenericType(pluginManagerT);
-                var getter = serviceInterfaceManager.GetMethod("Get", BindingFlags.Static | BindingFlags.Public);
-                if (getter == null) throw new Exception("Failed to get Get<Service<PluginManager>> method");
+            var serviceInterfaceManager = service1T.MakeGenericType(pluginManagerT);
+            var getter = serviceInterfaceManager.GetMethod("Get", BindingFlags.Static | BindingFlags.Public);
+            if (getter == null) throw new Exception("Failed to get Get<Service<PluginManager>> method");
 
-                var pluginManager = getter.Invoke(null, null);
+            var pluginManager = getter.Invoke(null, null);
 
-                if (pluginManager == null) throw new Exception("Failed to get PluginManager instance");
+            if (pluginManager == null) throw new Exception("Failed to get PluginManager instance");
 
-                var installedPluginsListField = pluginManager.GetType().GetField("installedPluginsList", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (installedPluginsListField == null) throw new Exception("Failed to get installedPluginsList field");
+            var installedPluginsListField = pluginManager.GetType().GetField("installedPluginsList", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (installedPluginsListField == null) throw new Exception("Failed to get installedPluginsList field");
 
-                _installedPluginsList = (IList?)installedPluginsListField.GetValue(pluginManager);
-                if (_installedPluginsList == null) throw new Exception("Failed to get installedPluginsList value");
-            }
+            _installedPluginsList = (IList?)installedPluginsListField.GetValue(pluginManager);
+            if (_installedPluginsList == null) throw new Exception("Failed to get installedPluginsList value");
         }
 
         private static bool GetAssemblyFromLoadContext(string name, object localPlugin, [NotNullWhen(true)] out Assembly? assembly)
@@ -238,31 +263,11 @@ namespace BasedArtisanReleaseFix
 
             return false;
         }
-
-        private static void SetLoadedPlugin(string internalName, object plugin)
-        {
-            PopulatePlugins();
-            PropertyInfo? internalNameProperty = null;
-
-            foreach (var v in _installedPluginsList)
-            {
-                internalNameProperty ??= v?.GetType().GetProperty("InternalName");
-
-                if (internalNameProperty == null) continue;
-                var installedInternalName = internalNameProperty.GetValue(v) as string;
-
-                if (installedInternalName == internalName && v != null)
-                {
-                    var t = v.GetType();
-                    while (t.Name != "LocalPlugin" && t.BaseType != null) t = t.BaseType;
-                    t.GetField("instance", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)?.SetValue(v, plugin);
-                }
-            }
-        }
         #endregion
 
         public void Dispose()
         {
+            State!.ClassJobChanged -= StateOnClassJobChanged;
             CommandManager!.RemoveHandler("/barf");
         }
     }
